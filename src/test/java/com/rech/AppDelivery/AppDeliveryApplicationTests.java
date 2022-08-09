552f509a8b638044b20244191340c0253aa0c86f
Rodrigo Rech <rodrigorech92@gmail.com>
2022-08-09 10:01:34 -0300

    Initial commit

---
+++ src/test/java/com/rech/AppDelivery/AppDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rech.AppDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
